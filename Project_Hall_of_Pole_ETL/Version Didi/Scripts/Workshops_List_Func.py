d932df966ff09232c58587d865fa41f51b14f888
Muhammed Al-Dalqamouni <hamudal.hd@gmail.com>
2023-05-02 00:49:38 +0200

    Didi V

--- Project_Hall_of_Pole_ETL/Version Didi/Scripts/Workshops_List_Func.py
+++ Project_Hall_of_Pole_ETL/Version Didi/Scripts/Workshops_List_Func.py
@@ -10,70 +10,47 @@ from sqlalchemy import text
 
 
 def workshop_list(url):
+
     # Send a GET request to the webpage
+    # url = 'https://www.eversports.de/sw/schoenheitstanz-studio'
     response = requests.get(url)
 
     # Parse the HTML content of the webpage
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Extract workshops dates
+    # Container Workshops
+    list_1 = soup.find_all(
+        'div', class_="discover-results marketplace-tile-container")
+
+    # Assuming you have already fetched the HTML content and parsed it using BeautifulSoup
     list_1 = soup.find_all('div', class_='marketplace-tile__date')
-    workshops_dates = [item.text for item in list_1]
 
-    # Extract workshop names
+    workshops_dates = []
+
+    # Loop through each div element and extract its text content
+    for item in list_1:
+        workshops_dates.append(item.text)
+
+    # Create list of names of the workshops
     workshop_names = []
+
     workshops = soup.find_all(
-        'div', class_='discover-results marketplace-tile-container')
-    try:
-        for item in workshops:
-            h4_tags = item.find_all('h4')
-            for tag in h4_tags:
-                workshop_names.append(tag.text)
-    except AttributeError:
-        print(f"No workshops found on {url}")
+        'div', class_="discover-results marketplace-tile-container")
 
-    # Extract pole studio name
-    name_soup = soup.find_all('div', class_='container discover-container')
-    polestudio_name = "0"
-    try:
-        for item in name_soup:
-            h1_tag = item.find_all('h1')
-            for h1 in h1_tag:
-                polestudio_name = h1.text
-        polestudio_name_cl = polestudio_name.split(" ")[0]
-    except AttributeError:
-        print(f"No pole studio name found on {url}")
-        polestudio_name_cl = 0
+    for item in workshops:
+        h4_tags = item.find_all('h4')
+        for tag in h4_tags:
+            workshop_names.append(tag.text)
 
-    # Extract price information
-    price_elements = soup.find_all('div', class_='marketplace-tile__price')
-    prices = []
-    try:
-        for item in price_elements:
-            if item.find('span'):
-                prices.append(item.find('span').text)
-            else:
-                prices.append('')
-    except AttributeError:
-        print(f"No price information found on {url}")
-
-    # Create a dictionary of the extracted information
-    workshop_dict = {'polestudio_name': polestudio_name_cl, 'workshop_name': workshop_names, 'date': workshops_dates,
-                     'price': prices}
-
-    # Convert the dictionary to a Pandas DataFrame and return it
-    return pd.DataFrame(workshop_dict)
-
-    # Assign a default value to polestudio_name
-    polestudio_name = "0"
+    name_soup = soup.find_all("div", class_="container discover-container")
+    name_soup
 
     for item in name_soup:
         h1_tag = item.find_all("h1")
         for h1 in h1_tag:
-            try:
-                polestudio_name_cl = polestudio_name.split(" ")[0]
-            except UnboundLocalError:
-                polestudio_name_cl = "Not Found"
+            polestudio_name = h1.text
+
+    polestudio_name_cl = polestudio_name.split(" ")[0]
 
     # Extract all div elements with class "marketplace-tile__price"
     price_elements = soup.find_all('div', class_='marketplace-tile__price')
@@ -111,7 +88,7 @@ def workshop_list(url):
 
     # create the directory if it doesn't exist
     # if not os.path.exists("Workshop_List_CSV"):
-    # os.makedirs("Workshop_List_CSV")
+        # os.makedirs("Workshop_List_CSV")
 
     # save the CSV file in the "CSV" directory
     # poleworkshop_list_df.to_csv(
