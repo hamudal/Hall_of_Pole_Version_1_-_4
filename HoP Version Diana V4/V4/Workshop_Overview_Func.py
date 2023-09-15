dd0050fa2c0201f4f42537bd532f471f70743300
Muhammed Al-Dalqamouni <hamudal.hd@gmail.com>
2023-09-15 14:59:02 +0200

    V4
    - needs try exept for modules

---
+++ HoP Version Diana V4/V4/Workshop_Overview_Func.py
@@ -0,0 +1,141 @@
+import os
+import re
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy import text
+
+
+def workshop_overview(url):
+
+    # Send a GET request to the webpage
+    response = requests.get(url)
+    # Parse the HTML content of the webpage
+    soup = BeautifulSoup(response.content, 'html.parser')
+    response
+
+    # extract workshop name
+    workshop_name = soup.find(
+        'div', class_='header-holder').find('h3').text.strip()
+
+    workshop_beschreibung = ''.join([p.text.strip() for p in soup.find(
+        'div', class_='marketplace-truncated-text-block js_truncate-block').find_all('p')])
+
+    # extract pole studio name
+    pole_studio = soup.find(
+        'a', class_='venue-name action-link-default').text.strip()
+
+    # extract pole studio address
+    try:
+        stadt = soup.find('div', class_='venue-location').text.strip()
+    except AttributeError:
+        stadt = 0
+
+    # extract pole studio street address
+    try:
+        strasse = soup.find('div', class_='venue-location').text.strip()
+    except AttributeError:
+        strasse = 0
+
+    # extract date
+    datum = soup.find('div', class_='date-holder').text.strip()
+
+    # extract time
+    uhrzeit = soup.find('div', class_='date-holder').text.strip()
+
+    # extract duration
+    dauer = soup.find('div', class_='date-holder').text.strip()
+
+    # extract level
+    try:
+        level = soup.find('div', class_='event-level').text.strip()
+    except AttributeError:
+        level = 0
+
+    # extract trainer
+    try:
+        trainer = soup.find('div', class_='trainer-name').text.strip()
+    except AttributeError:
+        trainer = 0
+
+    # extract trainer description
+    try:
+        trainer_beschreibung = soup.find(
+            'div', class_='about-teacher text').text.strip()
+    except:
+        trainer_beschreibung = 0
+
+    # extract price
+    preis = soup.find('div', class_='event-detail-price__price').text.strip()
+
+    # Reconstruct Eversports Pole Studio Webpage
+    a_tag = soup.find('a', {'class': 'venue-name'})
+
+    link_p2 = a_tag['href']
+    link_p1 = "https://www.eversports.de"
+    polestudio_eversports_seite = link_p1 + link_p2
+
+    # Regex names
+    workshop_name = re.sub(r'\W+', '_', workshop_name)
+
+    # create pandas DataFrame
+    workshop_overview_df = pd.DataFrame({
+        'Workshop Name': [workshop_name],
+        'Workshop Beschreibung': [workshop_beschreibung],
+        'PoleStudio': [pole_studio],
+        'Stadt': [stadt],
+        'Straße': [strasse],
+        'Datum': [datum],
+        'Uhrzeit': [uhrzeit],
+        'Dauer': [dauer],
+        'Level': [level],
+        'Trainer': [trainer],
+        'Trainer Beschreibung': [trainer_beschreibung],
+        'Preis': [preis],
+        'Workshop Everest Seite': url,
+        'PoleStudio Eversports Seite': polestudio_eversports_seite
+    })
+
+    workshop_overview_df['Version'] = datetime.now().strftime(
+        "%Y-%m-%d %H:%M:%S")
+
+    # Splitt the string to get the town
+    try:
+        workshop_overview_df["Stadt"] = stadt.split(",")[1].split(" ")[2]
+    except:
+        workshop_overview_df["Stadt"] = 0
+
+    # Splitt the string to get the street
+    try:
+        workshop_overview_df["Straße"] = strasse[5:-7]
+    except:
+        workshop_overview_df["Straße"] = 0
+
+    #
+    workshop_overview_df["Datum"] = datum[3:13]
+    #
+    workshop_overview_df["Uhrzeit"] = uhrzeit[14:29]
+
+    #
+    try:
+        workshop_overview_df["Dauer"] = dauer[-7:-1]
+    except:
+        workshop_overview_df["Dauer"] = 0
+    #
+    try:
+        workshop_overview_df["Level"] = level[5:]
+    except:
+        workshop_overview_df["Level"] = 0
+
+    # create the "CSV" directory if it does not exist
+    # if not os.path.exists("Workshop_CSV"):
+        # os.makedirs("Workshop_CSV")
+
+    # save the CSV file in the "CSV" directory
+    # workshop_overview_df.to_csv(
+    #     f"Workshop_CSV/{pole_studio}_{workshop_name}.csv", index=False)
+
+    return workshop_overview_df
---
+++ HoP Version Diana V4/V4/Workshops_List_Func.py
@@ -0,0 +1,100 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+import re
+from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy import text
+
+
+def workshop_list(url):
+
+    # Send a GET request to the webpage
+    # url = 'https://www.eversports.de/sw/schoenheitstanz-studio'
+    response = requests.get(url)
+
+    # Parse the HTML content of the webpage
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Container Workshops
+    list_1 = soup.find_all(
+        'div', class_="discover-results marketplace-tile-container")
+
+    # Assuming you have already fetched the HTML content and parsed it using BeautifulSoup
+    list_1 = soup.find_all('div', class_='marketplace-tile__date')
+
+    workshops_dates = []
+
+    # Loop through each div element and extract its text content
+    for item in list_1:
+        workshops_dates.append(item.text)
+
+    # Create list of names of the workshops
+    workshop_names = []
+
+    workshops = soup.find_all(
+        'div', class_="discover-results marketplace-tile-container")
+
+    for item in workshops:
+        h4_tags = item.find_all('h4')
+        for tag in h4_tags:
+            workshop_names.append(tag.text)
+
+    name_soup = soup.find_all("div", class_="container discover-container")
+    name_soup
+
+    for item in name_soup:
+        h1_tag = item.find_all("h1")
+        for h1 in h1_tag:
+            polestudio_name = h1.text
+
+    polestudio_name_cl = polestudio_name.split(" ")[0]
+
+    # Extract all div elements with class "marketplace-tile__price"
+    price_elements = soup.find_all('div', class_='marketplace-tile__price')
+
+    # Extract the text content of each price element
+    workshop_prices = [price.text.strip() for price in price_elements]
+
+    # find all links inside the container and create a list
+
+    links = [a['href'] for a in soup.find(
+        'div', class_='discover-results marketplace-tile-container').find_all('a')]
+
+    workshop_page_eversports = []
+    base_url = "https://www.eversports.de"
+
+    workshop_page_eversports = [base_url + link for link in links]
+    workshop_page_eversports
+
+    # SW URL
+
+
+    # create a dictionary from the given lists
+    poleworkshop_list_df = {
+        'Workshop Name': workshop_names,
+        'PoleStudio': polestudio_name_cl,
+        # 'Workshop Datum': workshops_dates,
+        # 'Workshop Preis': workshop_prices,
+        'Workshop Seite Eversports': workshop_page_eversports,
+
+    }
+
+    # create pandas dataframe
+    poleworkshop_list_df = pd.DataFrame.from_dict(poleworkshop_list_df)
+
+    # Add a new column to the DataFrame with the current date and time
+    poleworkshop_list_df['Version'] = datetime.now().strftime(
+        "%Y-%m-%d %H:%M:%S")
+
+    # create the directory if it doesn't exist
+    # if not os.path.exists("Workshop_List_CSV"):
+        # os.makedirs("Workshop_List_CSV")
+
+    # save the CSV file in the "CSV" directory
+    # poleworkshop_list_df.to_csv(
+    #     f"Workshop_List_CSV/{polestudio_name_cl}.csv", index=False)
+
+    return poleworkshop_list_df
